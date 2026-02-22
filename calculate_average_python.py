91fb881d1983150a028e5e6f56ec43c3b584aed6
LankyLuke97 <lukehowden@rocketmail.com>
2026-02-22 21:53:22 +0000

    Attempting to fix crash, now not thread-safe, and still runs out of memory

--- calculate_average_python.py
+++ calculate_average_python.py
@@ -10,8 +10,7 @@ from queue import Queue
 SEP = ord(';')
 END = ord('\n')
 
-def worker(mm: Any, ptr: int, end: int, process_queue: Queue) -> None:
-    results = {}
+def worker(mm: Any, ptr: int, end: int, results: Dict[str, List[float, float, float, int]], sorted_keys: List[str]) -> None:
     if ptr != 0:
         while mm[ptr] != END: ptr += 1
         ptr += 1
@@ -25,32 +24,14 @@ def worker(mm: Any, ptr: int, end: int, process_queue: Queue) -> None:
         while mm[ptr] != END: ptr += 1
         value = float(mm[seg_start:ptr])
         ptr += 1
-        if station not in results: results[station] = [100,0,-100,0]
+        if station not in results:
+            results[station] = [100,0,-100,0]
+            insort(sorted_keys, station)
         prev = results[station]
         if value < prev[0]: prev[0] = value
         if value > prev[2]: prev[2] = value
         prev[1] += value
         prev[3] += 1
-    process_queue.put(results)
-
-def combine(process_queue: Queue, num_blocks: int) -> Dict[str, tuple[float, float, float, int]]:
-    combined_results = {}
-    sorted_keys = []
-    num_blocks -= 1
-    while num_blocks:
-        num_blocks -= 1
-        result = process_queue.get()
-        for station, (min_, total, max_, num) in result.items():
-            if station not in combined_results: 
-                combined_results[station] = [100,0,-100,0]
-                insort(sorted_keys, station)
-            prev = combined_results[station]
-            if min_ < prev[0]: prev[0] = min_
-            if max_ > prev[2]: prev[2] = max_
-            prev[1] += total
-            prev[3] += num
-    return sorted_keys, combined_results
-
 
 profile_out = input('Please provide an output filename for the profiler: ')
 
@@ -60,7 +41,7 @@ with cProfile.Profile() as profile:
     i = 0
     block_size = 1024
 
-    input_file = Path('measurements.txt')
+    input_file = Path('measurements_short.txt')
     total_bytes = input_file.stat().st_size
     blocks = total_bytes / block_size
     if int(blocks) != blocks: blocks = int(blocks) + 1
@@ -68,22 +49,20 @@ with cProfile.Profile() as profile:
 
     with input_file.open(mode='rb') as file:
         mm = mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ)
-        process_queue = Queue()
-        stations = sorted_keys = None
+        stations = {}
+        sorted_keys = [] 
         futures = []
         i = 0
 
         with ThreadPoolExecutor() as executor:
-            future = executor.submit(combine, process_queue, blocks)
-            futures.append(future)
-            for block in range(blocks): futures.append(executor.submit(worker, mm, block * block_size, (block+1) * block_size, process_queue))
+            for block in range(blocks): futures.append(executor.submit(worker, mm, block * block_size, (block+1) * block_size, stations, sorted_keys))
             wait(futures)
             # for f in as_completed(futures):
             #     i += 1
             #     if i % 1_000 == 0: print(f'Processed {(i*block_size) // 1024} KB out of {total_bytes // 1024}')
             
-            sorted_keys, stations = future.result()
 
+    print(len(stations))
     with open('calculated_python.txt', mode='w', encoding='utf-8') as out_file:
         out_file.write('{') 
         out_file.write(', '.join([f'{str(k)}={stations[k][0]}/{round(stations[k][1] / stations[k][3], 1)}/{stations[k][2]}' for k in sorted_keys]))
